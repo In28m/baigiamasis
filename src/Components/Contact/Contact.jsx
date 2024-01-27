134c8480026405459d05eb2f996ed4bda69f82a3
In28 <ina.moisejeva@gmail.com>
2024-01-27 15:12:16 +0200

    contact page pristaikymas

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,26 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const Contact = () => {
+  const [message, setMessage] = useState('')
+
+  const handleChange = (event) => {
+    setMessage(event.target.value)
+  }
+
+  useEffect(() => {
+    const textarea = document.querySelector('.messageInput')
+    textarea.style.height = 'auto';
+    textarea.style.height = (textarea.scrollHeight) + 'px'
+  }, [message])
+
   return (
-   <section className='contactSection'>
-    <h1>CONTACT US</h1>
+    <section className='contactSection'>
+      <h1>CONTACT US</h1>
       <div className='Contcontainer'>
         <div className='contactH'>
-          <h1>Laamu Atoll, 
+          <h1>
+            Laamu Atoll,
             <br /> South Central Province,
             <br /> Maldives
-          </h1> 
-          <h3>INFO@MALDIVES.COM</h3> 
+          </h1>
+          <h3>INFO@MALDIVES.COM</h3>
           <h3>123-456-7890</h3>
-        </div> 
+        </div>
 
         <div className="contInput">
           <h3>Message Us</h3>
-            <input type="text" placeholder='name' />
-            <input type="number" placeholder='phone' />
-            <input type="text" placeholder='email' />
-            <input className='messageInput' type="text" placeholder='Your message' />
-        </div>   
+          <input type="text" placeholder='name' />
+          <input type="number" placeholder='phone' />
+          <input type="text" placeholder='email' />
+          <textarea
+            className='messageInput'
+            placeholder='Your message'
+            value={message}
+            onChange={handleChange}
+          />
+        </div>
       </div>
     </section>
   )
